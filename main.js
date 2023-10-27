cc7eba66588528619de058566f6d7cf4bc0a47cb
vivil1 <106763955+vivil1@users.noreply.github.com>
2023-10-27 16:14:04 -0300

    lhyg

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-function TocaSomPom(){
+function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom').onclick = TocaSomPom;
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
