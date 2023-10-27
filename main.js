4814752ffd15e2e62457ca8f424fedcfd22a16e0
vivil1 <106763955+vivil1@users.noreply.github.com>
2023-10-27 17:52:57 -0300

    çgui.b

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
+function tocaSomPom() {
+    document.querySelector('#som_tecla_pom').play();
+}
 
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
 function tocaSomClap() {
     document.querySelector('#som_tecla_clap').play();
 }
+
 document.querySelector('.tecla_clap').onclick = tocaSomClap;
 
 function tocaSomTim() {
@@ -20,7 +24,7 @@ document.querySelector('.tecla_puff').onclick = tocaSomPuff;
 
 function tocaSomSplash() {
     document.querySelector('#som_tecla_splash').play();
-} 
+}
 
 document.querySelector('.tecla_splash').onclick = tocaSomSplash;
 
